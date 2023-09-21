aef4fdf8c34a99977e8438494764a30ea5cd4bee
yousef <yousefsameh24@gmail.com>
2023-09-21 12:36:21 +0300

    create cronjob

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { Elysia } from "elysia";
+import { cron } from '@elysiajs/cron'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+
+app.get("/", () => "Hello Elysia")
+
+app.use(
+  cron({
+      name: 'heartbeat',
+      pattern: '*/10 * * * * *',
+      run() {
+          console.log('Heartbeat')
+      }
+  })
+)
+
+app.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
